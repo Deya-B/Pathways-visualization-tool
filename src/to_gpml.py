eaff0b4c6dedaeac897f6d4e8ce580154c77c7a7
DeyaB <deyanira.borroto@estudiante.uam.es>
2025-11-26 15:13:54 +0100

    catalytic and conversion interactions extracted

--- src/to_gpml.py
+++ src/to_gpml.py
@@ -39,7 +39,6 @@ import networkx as nx
 # ENZYME_OFFSET = 0.0     # distance from the anchor to the enzyme
 # ENZYME_STACK_GAP = 60.0 # separation between multiple enzymes on the same anchor
 
-
 ################################ NODE #########################################
 ## Description:
     # GPML DataNode (metabolite or enzyme). Contains the Node properties.
@@ -99,16 +98,51 @@ class Node:
             "Database": self.database,
             "ID": self.db_id
         })
-
-    # # Extract each row from tabular data and transform into a Node object
-    # @classmethod
-    # def from_row(cls, row, node_type_col, label_col, database_col, db_id_col):
-    #     label = row[label_col]
-    #     database = row[database_col]
-    #     db_id = row[db_id_col] 
-    #     return cls(label, database, db_id)
     
 
+
+############################## INTERACTION ####################################
+## Description:
+    # Represents conversions or catalysis from nodes
+    # Computes anchors for catalytic reactions
+## Attributes:
+        # source: Source Node
+        # target: Target Node: if target=conversion | None: if target=anchor
+        # type: interaction type (conversion, catalysis...)
+        # anchor_pos: (default_anchor_pos = 0.4) position for the anchor
+        # anchor_id: set for conversion > used as a destination in catalysis
+        # anchor_xy: calculate anchor coords (to be computed ONLY if coordinates exist)
+## Methods:
+
+class Interaction: 
+    default_anchor_pos = 0.4
+    def __init__(self, source, target, interaction_type, anchor_pos=None, anchor_id=None):
+        self.graph_id = idgenerator.new("interaction")
+        self.source = source                
+        self.target = target               
+        self.type = (interaction_type or "").lower()
+        self.anchor_pos = float(anchor_pos) if anchor_pos is not None else self.default_anchor_pos
+        self.anchor_id = anchor_id          
+        self._anchor_xy = None              # private: to be computed ONLY if coordinates exist
+
+        # self.ArrowHead = if enzyme is "mim-catalysis" else "mim-conversion"
+
+    def compute_anchor_xy(self):
+        """Compute anchor coordinates on the source-target line."""
+        if not self._can_compute_anchor():
+            return None
+        x1 = self.source.x; y1 = self.source.y + self.source.height/2.0                          
+        x2 = self.target.x; y2 = self.target.y - self.target.height/2.0
+        ax = x1 + self.anchor_pos * (x2 - x1)
+        ay = y1 + self.anchor_pos * (y2 - y1)
+        self._anchor_xy = (ax, ay)
+        return self._anchor_xy
+    
+    def to_gpml(self):
+        pass
+
+
+        
 ################################ MAIN #########################################
 
 def main(pathway_title, organism, ID_data_file, relations_file, delimiter="\t", ):
@@ -120,51 +154,130 @@ def main(pathway_title, organism, ID_data_file, relations_file, delimiter="\t", 
     id_data_df = pd.read_csv(ID_data_file, sep=delimiter, encoding="utf-8")
     id_col,db_col,name_col = id_data_df.columns[0:3] # extract column names
     
-    node_dict = {}  # Keep record of metabolite nodes already mapped
+    nodes_dict_info = {} # Keep nodes info
+    db_index = {} # Keep record of metabolite nodes already mapped (not to repeat)
+    interactions_list = []
+
     # Iterate over the rows as relations in tuples of three
-    print(relations_df.itertuples)
+    #  print(relations_df.itertuples)
     for row in relations_df.itertuples():
-        # Source
-        source_id = getattr(row, source_col) # Get ID
-        if source_id not in node_dict and pd.notnull(source_id):
+        # Get IDs
+        source_id = getattr(row, source_col)
+        target_id = getattr(row, target_col)
+        catal_id = getattr(row, catalyser_col)
+
+        # Build SOURCE Nodes
+        if pd.notnull(source_id) and source_id not in db_index:
             # Matching ID with that from id_data_df
             match_row = id_data_df[id_data_df[id_col] == source_id]
             if not match_row.empty:
                 info = match_row.iloc[0]
-                if is_wikipathways(source_id) or is_kegg(source_id):
-                    node_type = "Pathway"
-                else:
-                    node_type = "Metabolite"
+                is_pathway = is_wikipathways(source_id) or is_kegg(source_id)
+                node_type = "Pathway" if is_pathway else "Metabolite"
                 # Extract info from tabular data and transform into a Node object
                 node = Node(node_type, info[name_col], info[db_col], source_id)
-                node_dict[source_id] = node
-    
-        # Target
-        target_id = getattr(row, target_col) # Get ID
-        if target_id not in node_dict and pd.notnull(target_id):
+                nodes_dict_info[node.graph_id] = node
+                # record that this metabolite is already mapped
+                db_index[source_id] = node.graph_id
+
+        # Build TARGET Nodes
+        if pd.notnull(target_id) and target_id not in db_index:
             match_row = id_data_df[id_data_df[id_col] == target_id]
             if not match_row.empty:
                 info = match_row.iloc[0]
-                if is_wikipathways(target_id) or is_kegg(target_id):
-                    node_type = "Pathway"
-                else:
-                    node_type = "Metabolite"
-                node = Node(node_type, info[name_col], info[db_col], target_id)
-                node_dict[target_id] = node
+                is_pathway = is_wikipathways(target_id) or is_kegg(target_id)
+                node_type = "Pathway" if is_pathway else "Metabolite"
+                node = Node(
+                    node_type, info[name_col], info[db_col], target_id
+                    )
+                nodes_dict_info[node.graph_id] = node
+                # record that this metabolite is already mapped
+                db_index[target_id] = node.graph_id
 
-        # Catalyser (always create new, even if repeated)
-        catal_id = getattr(row, catalyser_col)
+        # Interactions
+        source_node = nodes_dict_info.get(db_index.get(source_id))
+        target_node = nodes_dict_info.get(db_index.get(target_id))
+        # print("source:", source_node.graph_id, "target:", target_node.graph_id)
+
+        if source_node and target_node:
+            conversion = Interaction(
+                source=source_node.graph_id, target=target_node.graph_id, 
+                interaction_type="mim-conversion"
+                )
+            interactions_list.append(conversion)
+            
+
+        # Build CATALYSER Nodes (store multiple nodes even if repeated)
         if pd.notnull(catal_id):
+            catal_node = None
             match_row = id_data_df[id_data_df[id_col] == catal_id]
             if not match_row.empty:
                 info = match_row.iloc[0]
-                node = Node("GeneProduct", info[name_col], info[db_col], catal_id)
-                node_dict[catal_id] = node
+                catal_node = Node(
+                    "GeneProduct", info[name_col], info[db_col], catal_id
+                    )
+                nodes_dict_info[catal_node.graph_id] = catal_node
+            catal_inter = Interaction(
+                source=catal_node.graph_id, target=None, interaction_type="mim-catalysis"
+                )  # target -> the anchor of a conversion must be assigned
+            interactions_list.append(catal_inter)
 
-    
-    for key, node in node_dict.items():
-        print(f"{key}: {vars(node)}")  # vars() returns the attributes as a dict
+
+    # for key, node in nodes_dict_info.items():
+    #     print(f"{key}: {vars(node)}")  # vars() returns the attributes as a dict
         
+    for inter in interactions_list:
+        print(f"{inter.source} → {inter.target}: {vars(inter)}")
+
+
+
+############################# LAYOUT CREATION #################################
+
+## PASAR A AQUÍ todo LO RELATIVO A COORDENADAS Y LAYOUT...
+
+class Layout:
+    def __init__(self, nodes, interactions):
+        self.nodes = nodes          # Dict of nodes, keyed by graph_id
+        self.interactions = interactions  # List of Interaction objects
+        self._laid_out = False
+
+    # def assign_layout(self):
+    #     # A) Skeleton graph (metabolites only)
+    #     G = nx.Graph()
+    #     for n in self.nodes.values():
+    #         if n.node_type.lower() == "enzyme":
+    #             continue
+    #         G.add_node(n.graph_id)
+    #     for e in self.interactions:
+    #         if e.type == "conversion":
+    #             G.add_edge(e.source.graph_id, e.target.graph_id)
+
+    #     # B) BFS layering (topology -> rows)
+    #     layers = list(nx.bfs_layers(G, [next(iter(G.nodes))])) if G.number_of_nodes() else []
+
+    #     # C) Place metabolites row-by-row (grid)
+    #     k_max = max((len(L) for L in layers), default=1)
+    #     span_max = max(0, (k_max - 1) * COL_GAP)
+
+    #     placed = set()
+    #     for ly, layer_nodes in enumerate(layers):
+    #         k = len(layer_nodes)
+    #         span = max(0, (k - 1) * COL_GAP)
+    #         start_x = BOARD_MARGIN + (span_max - span) / 2.0
+    #         y = BOARD_MARGIN + ly * LAYER_GAP
+    #         for i, gid in enumerate(layer_nodes):
+    #             node = self.nodes[gid]
+    #             x = start_x + i * COL_GAP
+    #             node.coords(x, y)
+    #             placed.add(gid)
+
+    #     # D) Temporarily place rest (enzymes/others) on extra row
+    #     rest = [n for gid, n in self.nodes.items() if gid not in placed]
+    #     for j, node in enumerate(rest):
+    #         node.coords(BOARD_MARGIN + j * COL_GAP, BOARD_MARGIN + (len(layers) + 1) * LAYER_GAP)
+
+    #     self._laid_out = True
+
 
 ########################### HELPER FUNCIONS ###################################
 
@@ -176,7 +289,7 @@ def is_wikipathways(id_str):
 def is_kegg(id_str):
     """Check if source/target ID match Pathway WikiPathways database, 
     which has the form hsa00120, for example"""
-    return re.match(r'^\w{2,4}\d{5}$', str(id_str)) is not None
+    return re.match(r'^[a-zA-Z]{2,4}\d{5}$', str(id_str)) is not None
 
 
 
@@ -212,6 +325,7 @@ class IDGenerator:
         k = (kind or "").lower()
         if k in {"enzyme", "geneproduct", "gene", "protein", "e"}: return "e"
         if k in {"metabolite", "compound", "smallmolecule", "m"}:  return "m"
+        if k in {"pathway"}:                                       return "p"
         if k in {"anchor", "a"}:                                   return "a"
         if k in {"interaction", "edge", "i"}:                      return "i"
         return "n"
@@ -223,55 +337,6 @@ class IDGenerator:
 
 # initiation of an instance for this class
 idgenerator = IDGenerator()
-
-
-############################## INTERACTION ####################################
-## Description:
-    # Represents conversions or catalysis from nodes
-    # Computes anchors for catalytic reactions
-## Attributes:
-        # source: Source Node
-        # target: Target Node: if target=conversion | None: if target=anchor
-        # type: interaction type (conversion, catalysis...)
-        # anchor_pos: (default_anchor_pos = 0.4) position for the anchor
-        # anchor_id: set for conversion > used as a destination in catalysis
-        # anchor_xy: calculate anchor coords (to be computed ONLY if coordinates exist)
-## Methods:
-
-# class Interaction: 
-#     default_anchor_pos = 0.4
-#     def __init__(self, source, target, interaction_type, anchor_pos=None, anchor_id=None):
-#         self.source = source                
-#         self.target = target               
-#         self.interaction_type = (interaction_type or "").lower()
-#         self.anchor_pos = float(anchor_pos) if anchor_pos is not None else self.default_anchor_pos
-#         self.anchor_id = anchor_id          
-#         self._anchor_xy = None              # private: to be computed ONLY if coordinates exist
-
-
-#     def compute_anchor_xy(self):
-#         """Compute anchor coordinates on the source-target line."""
-#         if not self._can_compute_anchor():
-#             return None
-#         x1 = self.source.x; y1 = self.source.y + self.source.height/2.0                          
-#         x2 = self.target.x; y2 = self.target.y - self.target.height/2.0
-#         ax = x1 + self.anchor_pos * (x2 - x1)
-#         ay = y1 + self.anchor_pos * (y2 - y1)
-#         self._anchor_xy = (ax, ay)
-#         return self._anchor_xy
-    
-#     def to_gpml(self):
-#         pass
-
-
-
-############################# LAYOUT CREATION #################################
-
-## PASAR A AQUÍ todo LO RELATIVO A COORDENADAS Y LAYOUT...
-
-class Layout:
-    def __init__(self):
-        pass
 
 
 ############################## XML BUILD #####################################
@@ -306,10 +371,16 @@ if __name__ == "__main__":
     main(pathway_title, organism, ID_data_file, relations_file)
 
 
-## NOTES
+
+## NOTES:
+
 # default delimiter="\t" 
-    # can be changed as a 3rd parameter. Example: 
-        # TSVParser(ID_data_file, relations_file, delimiter=";")
+    # can be changed as a 5th parameter. Example: 
+        # main(pathway_title, organism, ID_data_file, relations_file, delimiter=";")
+
 # relations_file 
     # must have an id for the NODE > if this specific component is not 
     # found in a DB, then an ID such as metabo1,enz1... must be provided.
+
+# IMPORTANT: Every ID from the relations file must be also present in the 
+#            ID_data_file provided, otherwise this wont appear in the pathway
